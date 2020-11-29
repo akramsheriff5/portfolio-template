efc227aad38e4254b955e7d3fd9f2bd56d8df166
akramsheriff05 <akramsheriff05042000@gmail.com>
2020-11-29 19:53:19 +0530

    visitor tweet added

--- App_Todo/models.py
+++ App_Todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class tweets(models.Model):
+   tweet = models.CharField(max_length=200)
+   time = models.DateTimeField(auto_now=True)
+   name = models.CharField(max_length=100)
--- App_Todo/views.py
+++ App_Todo/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import Q
+
+# database models
+from .models import tweets
 # Create your views here.
 
 def index(request):
    return render(request, 'index.html')
+
+def tweet_data(request):
+   if request.method == "POST":
+      tweets_info = tweets(tweet = request.POST['tweet'],name = request.POST['name'])
+      tweets_info.save()
+
+   elif request.method == 'GET':
+        tweeetData = serializers.serialize('json', tweets.objects.all())
+        return HttpResponse(json.dumps(tweetData), content_type='application/json')
